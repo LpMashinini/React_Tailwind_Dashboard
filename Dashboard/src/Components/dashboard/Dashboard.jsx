25264ec5b705aab4f932c2ab0eaf13e795d16a59
LpMashinini <lebogangp84@gmail.com>
2026-01-21 01:45:29 +0200

    Completed chart feature for dashboard

--- Dashboard/src/Components/dashboard/Dashboard.jsx
+++ Dashboard/src/Components/dashboard/Dashboard.jsx
@@ -1,10 +1,13 @@
 import StatsGrid from "../dashboard/StatsGrid"
+import ChartSection from "./ChartSection"
 const Dashboard = () => {
   return (
     <div className="space-y-6">
       
       {/* stats Grid */}
       <StatsGrid/>
+      {/* Charts Section */}
+      <ChartSection/>
     </div>
   )
 }
