e4bc3d618ea0d6ff2c44445019c6c7c8aa20b437
LpMashinini <lebogangp84@gmail.com>
2026-01-21 14:45:55 +0200

    Completed pie chart design feature

--- Dashboard/src/Components/dashboard/ChartSection.jsx
+++ Dashboard/src/Components/dashboard/ChartSection.jsx
@@ -1,4 +1,5 @@
 import RevenueChart from "./RevenueChart"
+import SalesChart from "./SalesChart"
 
 const ChartSection = () => {
   return (
@@ -6,6 +7,9 @@ const ChartSection = () => {
     <div className="grid grid-cols-1 xl:grid-cols-3 gap-6">
         <div className="xl:col-span-2">
             <RevenueChart/>
+        </div>
+        <div className="space-y-6">
+            <SalesChart/>
         </div>
     </div>
   )
